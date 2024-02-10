b9deaa00478d9a400f99e845537e64ec6bbbed6a
ranaamr22 <ranaamr169@yahoo.com>
2024-02-10 11:11:39 +0200

    feat: add todo class

---
+++ todo-app/Models/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations
+namespace todo_app.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Please enter a description")]
+        public string Description { get; set; } = String.Empty;
+
+        [Required(ErrorMessage = "Please enter a due date.")]
+        public DateTime? DueDate { get; set; }
+
+    }
+
+}
