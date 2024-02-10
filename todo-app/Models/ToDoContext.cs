aea8ce01a935eb963e87a7ded973014af0d617fa
ranaamr22 <ranaamr169@yahoo.com>
2024-02-10 14:37:03 +0200

    feat: add ToDoContext class

---
+++ todo-app/Models/ToDoContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace todo_app.Models
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options) : base(options) { }
+    }
+}
