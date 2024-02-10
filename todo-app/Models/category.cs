3949189b128fa3c3c36f85bcf5c26bc6edb5fb5d
ranaamr22 <ranaamr169@yahoo.com>
2024-02-10 11:10:58 +0200

    feat: add category class

---
+++ todo-app/Models/category.cs
@@ -0,0 +1,8 @@
+﻿namespace todo_app.Models
+{
+    public class Category
+    {
+        public string CategoryId { get; set; } = String.Empty;
+        public string Name { get; set; } = String.Empty;
+    }
+}
